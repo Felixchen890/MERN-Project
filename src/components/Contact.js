2ecec553eecebf2f061f43d61c3d3f462c602022
pchen05 <chenpingyue14@gmail.com>
2024-04-28 15:39:37 -0700

    Add client form submit functionality

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,47 +1,81 @@
-import React from 'react';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React, { useState } from "react";
+import axios from "axios";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import './Contact.css';
+import "./Contact.css";
 
 function Contact() {
-	return (
-		<div>
-			<div className="container">
-				<div className="first-layer">
-					<h1>Contact Us</h1>
-					<div className="input-section">
-						<div className="left-section">
-							<input type="text" placeholder="Name" />
-							<input type="email" placeholder="Email" />
-							<input type="tel" placeholder="Phone" />
-						</div>
-						<div className="right-section">
-							<textarea placeholder="Message" />
-						</div>
-					</div>
-					<button type="submit">Submit</button>
-				</div>
-				<div className="second-layer">
-					<div className="map-container">
-						<iframe
-							title="Google Map Preview"
-							src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d435181.11373642654!2d-74.25986499544389!3d40.69714941975367!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c2594b9c5a6c1d%3A0xa2f9a0bcb4f6b5b2!2sNew%20York%2C%20NY!5e0!3m2!1sen!2sus!4v1653464850583!5m2!1sen!2sus"
-							width="400"
-							height="300"
-							style={{ border: 0 }}
-							allowfullscreen=""
-							loading="lazy"
-						/>
-					</div>
-				</div>
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    message: "",
+  });
 
-                <Footer />
-				
-			</div>
-		</div>
-	);
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    axios
+      .post("http://localhost:9100/submit", formData)
+      .then((response) => {
+        console.log("form submitted successfully:", response.data);
+		alert("Form submitted successfully! ");
+		setFormData({
+			name: '',
+			email: '',
+			phone: '',
+			message: ''
+		});
+      })
+      .catch((error) => {
+        console.error("form submitted not successfully:", error);
+      });
+  };
+
+  return (
+    <div>
+      <div className="container">
+        <div className="first-layer">
+          <h1>Contact Us</h1>
+          <form onSubmit={handleSubmit}>
+            <div className="input-section">
+              <div className="left-section">
+                <input type="text" placeholder="Name" name="name" value={formData.name} onChange={handleInputChange}/>
+                <input type="email" placeholder="Email" name="email" value={formData.email} onChange={handleInputChange}/>
+                <input type="tel" placeholder="Phone" name="phone" value={formData.phone} onChange={handleInputChange}/>
+              </div>
+              <div className="right-section">
+                <textarea placeholder="Message" name="message" value={formData.message} onChange={handleInputChange}/>
+              </div>
+            </div>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+        <div className="second-layer">
+          <div className="map-container">
+            <iframe
+              title="Google Map Preview"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d435181.11373642654!2d-74.25986499544389!3d40.69714941975367!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c2594b9c5a6c1d%3A0xa2f9a0bcb4f6b5b2!2sNew%20York%2C%20NY!5e0!3m2!1sen!2sus!4v1653464850583!5m2!1sen!2sus"
+              width="400"
+              height="300"
+              style={{ border: 0 }}
+              allowfullscreen=""
+              loading="lazy"
+            />
+          </div>
+        </div>
+
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,58 +1,78 @@
-import React from 'react';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import {
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import './Home.css';
+import "./Home.css";
 
 function Home() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:9100/products")
+      .then((response) => {
+        setProducts(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  const groupedProducts = [];
+  const itemsPerRow = 3;
+  for (let i = 0; i<products.length; i+= itemsPerRow) {
+    groupedProducts.push(products.slice(i, i + itemsPerRow));
+  }
   return (
-      <div>
-        <div className="first-layer">
-          <h1>my banner</h1>
-          <p>my text</p>
+    <div>
+      <div className="first-layer">
+        <h1>Find Your Favourite Anime Characters!</h1>
+        <p>my text</p>
+      </div>
+      <br />
+      <div className="second-layer">
+        <div className="second-layer-left-item">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXYujgMGETU_Jl-MhilQOyEkIBaoRZbfZRtObwOFsM5w&s"
+            alt="Image"
+          />
         </div>
-        <br />
-        <div className="second-layer">
-          <div className="second-layer-left-item">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXYujgMGETU_Jl-MhilQOyEkIBaoRZbfZRtObwOFsM5w&s" alt="Image" />
-          </div>
-          <div className="second-layer-right-item">
-            <h1>my title right</h1>
-            <p>my text right</p>
-          </div>
+        <div className="second-layer-right-item">
+          <h1>my title right</h1>
+          <p>my text right</p>
         </div>
-        <div className = "third-layer">
-        <table>
+      </div>
+      <div className="third-layer">
+      <table>
           <tbody>
-            <tr>
-              <td>Cell 1</td>
-              <td>Cell 2</td>
-              <td>Cell 3</td>
-              <td>Cell 4</td>
-            </tr>
-            <tr>
-              <td>Cell 5</td>
-              <td>Cell 6</td>
-              <td>Cell 7</td>
-              <td>Cell 8</td>
-            </tr>
-            <tr>
-              <td>Cell 9</td>
-              <td>Cell 10</td>
-              <td>Cell 11</td>
-              <td>Cell 12</td>
-            </tr>
+            {groupedProducts.map((row, rowIndex) => (
+              <tr key={rowIndex}>
+                {row.map((product, columnIndex) => (
+                  <td key={columnIndex}>
+                    <img
+                        src={product.imageURL}
+                        alt={`产品 ${rowIndex * itemsPerRow + columnIndex + 1}`}
+                        style={{ width: "100%", height: "auto" }}
+                      />
+                      <p className="product-name" style={{ textAlign: "center", marginTop: "5px"}}>
+                        {product.name}
+                      </p>
+                  </td>
+                ))}
+                {/* 如果一行不足三个产品，填充空单元格 */}
+                {row.length < itemsPerRow && (
+                  <td colSpan={itemsPerRow - row.length}></td>
+                )}
+              </tr>
+            ))}
           </tbody>
         </table>
-        </div>
-
-        <Footer />
-
       </div>
+
+      <Footer />
+    </div>
   );
 }
 
